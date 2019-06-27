6a014b339a5e5c1d954ece753096cc64486c2732
Jorge Castro <jorge.castro.c@gmail.com>
2019-06-26 21:53:54 -0400

    ok.

--- WebFormEjemplo1/dal/ComprasDAL.cs
+++ WebFormEjemplo1/dal/ComprasDAL.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using WebFormEjemplo1.base2;
+using WebFormEjemplo1.dto;
 
 namespace WebFormEjemplo1.dal
 {
@@ -28,7 +29,55 @@ namespace WebFormEjemplo1.dal
                 grupo.SaveChanges();
             }
         }
+        public static void Modificar(Compras compra)
+        {
+            using (var grupo = new Model1())
+            {
+                Compras compraAntigua = grupo.Compras
+                    .FirstOrDefault(c => c.CompraId == compra.CompraId); // prpxy
 
+                compraAntigua.CompraId=compra.CompraId;
+                compraAntigua.Cantidad=compra.Cantidad;
+                compraAntigua.PrecioUnitario=compra.PrecioUnitario;
+                compraAntigua.Producto=compra.Producto;
+
+                grupo.SaveChanges();
+            }
+        }
+
+        public static int Contar()
+        {
+            int total=0;
+            using (var grupo = new Model1())
+            {
+                total=grupo.Compras.Count();
+            }
+            return total;
+        }
+        public static List<Compras> Listar2()
+        {
+            var resultado = new List<Compras>();
+            using (var grupo = new Model1())
+            {
+                resultado=grupo.Database.SqlQuery<Compras>("select * from compras")
+                    .ToList();
+            }
+            return resultado;
+        }
+
+        public static List<CompraDto> Listar3()
+        {
+            var resultado = new List<CompraDto>();
+            string sql= @"select country.country Pais,count(*) Cantidad from city 
+                        inner join country on city.country_id = country.country_id
+                        group by country.country";
+            using (var grupo = new Model1())
+            {
+                resultado = grupo.Database.SqlQuery<CompraDto>(sql)
+                    .ToList();
+            }
+            return resultado;
+        }
 
         public static void Insertar(Compras compra)
         {
---
+++ WebFormEjemplo1/dto/CompraDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFormEjemplo1.dto
+{
+    // DTO es una clase de modelo para transportar informacion
+    // Data Transfer Object
+    // No es para ingresar manualmente
+    // Value Object  VO
+
+    public class CompraDto
+    {
+        public string Pais { set; get;}
+        public int Cantidad { set; get;}
+    }
+}
